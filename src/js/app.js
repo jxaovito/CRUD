5a810ccb49eef920a25118428a9b714f44fb5a4a
jxaovito <joaovitoorvieiraborba@gmail.com>
2023-12-20 20:59:21 -0300

    crud finished, code cleaned

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,11 @@
 const { connect } = require('./db.js')
 const connection = connect();
 const express = require('express');
-const { list } = require('./controllers/controller.js');
 const port = 3000;
 const router = require('./routers/router.js');
 const app = express();
 const bodyParser = require('body-parser');
 const handlebars = require('express-handlebars');
-const { engine } = require ('express-handlebars');
-
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,10 +13,10 @@ app.engine('handlebars', handlebars.engine({defaultLayout: false}));
 app.set('view engine','handlebars');
 app.use(express.json());
 
-// app.set('views', __dirname );
 
 app.use('/src', express.static('../src/'));
 app.use(router);
+
 
     app.listen(port, () => {
         console.log("Server is running on port 3000");
--- src/js/routers/router.js
+++ src/js/routers/router.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 const router = express.Router()
-const { list } = require('../controllers/controller.js');
 const fs = require('fs');
 const bodyParser = require('body-parser');
 const { connect } = require('../db.js');
@@ -9,54 +8,57 @@ const { connect } = require('../db.js');
 router.use(express.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-router.get("/", (req, res) => { 
-    const filePath = path.join(__dirname, "../../../index.html");
+// Rota da página inicial - cadastro
+router.get("/", (req, res) => {
+    const filePath = path.join(__dirname, "../../../index.handlebars");
     fs.readFile(filePath, 'utf8', (err, data) => {
         res.send(data);
-})});
+    })
+});
 
-router.post("/cadastro", async (req, res, next) => {
-async function create() {
+// Rota que confirma o cadastro do produto
+router.post("/register", async (req, res, next) => {
     const connection = await connect();
     const result = await connection.query(`INSERT INTO produtos(id_produto, nome_produto, descricao_produto, preco_produto, quantidade_produto) VALUES ("", "${req.body.productName}", "${req.body.productDesc}", "${req.body.productPrice}", "${req.body.productQuantity}")`);
+    res.redirect('/');
+});
 
-    res.json(await create(req.body));
- 
-}});
-
-router.get("/form", async (req, res) => {  
+// Rota que gera a lista dos produtos
+router.get("/form", async (req, res) => {
     const connection = await connect();
     const [rows] = await connection.query('SELECT * FROM produtos');
-    res.render('itemList.handlebars', {produtos: rows});
-    }) ;
+    res.render('itemList.handlebars', { produtos: rows });
+});
 
-    router.get("/updateProduct/:id_produto", async (req, res) => {  
-        const connection = await connect();
-        const [rows] = await connection.query(`SELECT * FROM produtos WHERE id_produto = "${req.params.id_produto}"`);
-    
-        res.render('updateForm.handlebars', { produtos: rows[0] });
-        }) 
+// Rota que reconhece o produto pelo id ao editar
+router.get("/updateProduct/:id_produto", async (req, res) => {
+    const connection = await connect();
+    const [rows] = await connection.query(`SELECT * FROM produtos WHERE id_produto = "${req.params.id_produto}"`);
 
+    res.render('updateForm.handlebars', { produtos: rows[0] });
+})
 
-    router.post("/updateProduct/:id_produto", async (req, res) => {
-        const connection = await connect();
-        const result = await connection.query(`UPDATE produtos SET nome_produto = "${req.body.productName}", descricao_produto = "${req.body.productDesc}", preco_produto = "${req.body.productPrice}", quantidade_produto = "${req.body.productQuantity}" WHERE id_produto = "${req.params.id_produto}"`).then(function(){
-            console.log("Cadastro atualizado");
-        }).catch(function(error){
-            console.log("Cadastro não atualizado");
-        })
-        res.redirect('/');
+// Rota que realiza a atualização do cadastro
+router.post("/updateProduct/:id_produto", async (req, res) => {
+    const connection = await connect();
+    const result = await connection.query(`UPDATE produtos SET nome_produto = "${req.body.productName}", descricao_produto = "${req.body.productDesc}", preco_produto = "${req.body.productPrice}", quantidade_produto = "${req.body.productQuantity}" WHERE id_produto = "${req.params.id_produto}"`).then(function () {
+        console.log("Cadastro atualizado");
+    }).catch(function (error) {
+        console.log("Cadastro não atualizado");
     })
+    res.redirect('/form');
+})
 
-        router.get("/delProduct/:id_produto", async (req, res) => {  
-            const connection = await connect();
-            const del = await connection.query(`DELETE FROM produtos WHERE id_produto = "${req.params.id_produto}"`).then(function(){
-                console.log("Produto excluído");
-            }).catch(function(error){
-                console.log("Produto não excluído");
-            })
-            res.redirect('/');
-            }) 
+// Rota que deleta o cadastro
+router.get("/delProduct/:id_produto", async (req, res) => {
+    const connection = await connect();
+    const del = await connection.query(`DELETE FROM produtos WHERE id_produto = "${req.params.id_produto}"`).then(function () {
+        console.log("Produto excluído");
+    }).catch(function (error) {
+        console.log("Produto não excluído");
+    })
+    res.redirect('/form');
+})
 
 
 module.exports = router;
